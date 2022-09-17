7f4292e5715fcc1f91c2576b1d608eeec883e2a6
ramonfc <monchouam@gmail.com>
2022-09-16 19:40:39 -0500

    added content about the video

--- w01/main.js
+++ w01/main.js
@@ -1,36 +1,55 @@
-const exercisesNotes = [
-    { label: "Story Editor: Using Local Storage", url: "./story_editor.html" },
+function main(){
+    const exercisesNotes = [
+        { label: "Story Editor: Using Local Storage", url: "./story_editor.html" },
+        {
+            label: `Local Storage: <br>
+        Save information: localStorage.setItem(key, value) <br>
+        Retrieve information: localStorage.getItem(key)`
+        },
+        {
+            label: `Master Mobile UX:
+        <ul>
+        <li>Desing first for the smallest and least powerful device</li>
+        <li>Start small and scale up</li>
+        <li>Move the primary controls to the bottom of the app</li>
+        <li>Think in user friendly design</li>
+        </ul>`
+        }
+    ];    
     
-  ];
-
-
-listWeeklyItems(exercisesNotes, "weekList");
-
-
+    listWeeklyItems(exercisesNotes, "weekList");
+}
 
 
 
 function listWeeklyItems(weekElements, listIdName) {
-  
+
     const ol = document.querySelector(`#${listIdName}`);
 
-    if(ol){
-        weekElements.forEach(element =>{
-         
-            const anchor = document.createElement("a");
-            anchor.innerHTML = element.label;
-            anchor.href = element.url;
-            // anchor.target = "_blank";
+    if (ol) {
+        weekElements.forEach(element => {
 
             const li = document.createElement("li");
-            li.appendChild(anchor);
+            
+            if (!element.url) {
+                li.innerHTML = element.label
+            }
+             else {
+                const anchor = document.createElement("a");
+                anchor.innerHTML = element.label;
+                anchor.href = element.url;
+                // anchor.target = "_blank";            
+                li.appendChild(anchor);
+            }
 
 
             ol.appendChild(li);
         });
     }
-
 }
 
 
 
+
+
+main();
