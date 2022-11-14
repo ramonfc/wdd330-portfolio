2762c4aeb4a7800c89675e781822f807e29469e3
ramonfc <monchouam@gmail.com>
2022-11-14 15:25:20 -0500

    changing js to make down the selected letter

--- w09/tw9/main.js
+++ w09/tw9/main.js
@@ -4,16 +4,24 @@ const arrayAudios = Array.from(audios);
 
 const keys = document.querySelectorAll(".key");
 
+function bajar(key, amount) {
+    key.style.transform = `translateY(${amount}px)`;
+    window.requestAnimationFrame(bajar);
+}
+let count = 1;
 addEventListener("keypress", e=>{
+    
     arrayAudios.forEach((audio, idx) =>{
         audio.pause();
         audio.currentTime = 0;
         if(audio.dataset.key == e.keyCode){
             audio.play();
-
+            count++; 
             audio.addEventListener('play', (event) => {
                 keys[idx].classList.add("playing");
-                keys[idx].classList.add("moving");
+                // keys[idx].classList.add("moving");
+                let id = requestAnimationFrame(bajar(keys[idx], 10*count));
+
             });
             
             audio.addEventListener('ended', (event) => {
@@ -23,7 +31,8 @@ addEventListener("keypress", e=>{
             audio.addEventListener('pause', (event) => {
                 keys[idx].classList.remove("playing");
             }); 
-            
+         
+         if(count ===11) count = 0;  
         }
         return
     });
